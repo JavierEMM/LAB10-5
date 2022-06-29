d0ed51f5e301c332f89e57d9262a6f06359d99a1
Diego Vargas <a20191027@pucp.edu.pe>
2022-06-28 19:42:10 -0500

    crear mascota

--- index.js
+++ index.js
@@ -18,3 +18,43 @@ const multerApp = multer();
 const app = express();
 
 app.use(express.static('public'));
+
+
+app.post("/mascota/create", bodyParser.json(), (req, res) => {
+    let nombre = req.body.nombre;
+    let anho = req.body.anho;
+    let historia = req.body.historia;
+    let observaciones = req.body.observaciones;
+    let sexo = req.body.sexo;
+    let raza_especie_idraza = req.body.raza_especie_idraza;
+    let raza_otros = req.body.raza_otros;
+    let cuenta_idcuenta = req.body.cuenta_idcuenta;
+
+    let sql = "insert into mascota SET ?";
+    let params = {
+        nombre: nombre,
+        anho: anho,
+        historia: historia,
+        observaciones: observaciones,
+        sexo: sexo,
+        raza_especie_idraza: raza_especie_idraza,
+        raza_otros: raza_otros,
+        cuenta_idcuenta: cuenta_idcuenta,
+    };
+
+    conn.query(sql, params, (e) => {
+        if (e) throw e;
+
+        conn.query("select * from mascota", (err, resultado) => {
+            if (err) throw err;
+            res.json(resultado);
+        });
+
+    });
+
+});
+
+
+app.listen(3000, () => {
+    console.log("servidor corriendo");
+});
